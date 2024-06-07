aabf99b622989cde3944d52c1ccd4891ed80ec68
RitikBora <ritik.bora@xecurify.com>
2024-06-07 15:22:49 +0530

    education done

--- frontend/src/components/sections/Education.tsx
+++ frontend/src/components/sections/Education.tsx
@@ -1,8 +1,61 @@
+import { EducationTimeLine } from '../timeline/EducationTimeLine';
+import { TimeLine } from '../timeline/TimeLine';
+
+
+
+
+
 export const Education = () =>
 {
     return (
-        <div>
-             Education
-        </div>
+       <div className='m-28 mt-16 grid grid-cols-7 '>
+            <div className='col-span-3 flex justify-end items-center '>
+                    <EducationItem
+                        date="Jan, 2021 - Present"
+                        school="Computer Engineering"
+                        degree="CPC Academy Pune, India"
+                        sgpa="9.8"
+                        style={{left : '462px' , top: '200px'}}
+                    />
+                     <EducationItem
+                        date="Jan, 2021 - Present"
+                        school="Computer Engineering"
+                        degree="CPC Academy Pune, India"
+                        sgpa="9.8"
+                        style={{left : '462px' , bottom: "20px"}}
+                    />
+                   
+            </div>
+            <div className='col-span-1  flex justify-center'>
+                <EducationTimeLine/>
+            </div>
+            <div className='col-span-3' >
+                  <EducationItem
+                        date="Jan, 2021 - Present"
+                        school="Computer Engineering"
+                        degree="CPC Academy Pune, India"
+                        sgpa="9.8"
+                        style={{right : '462px' , bottom: "190px"}}
+                    />
+            </div>
+       </div>
     )
 }
+
+interface EducationItemProps {
+  date: string;
+  school: string;
+  degree: string;
+  sgpa: string;
+  style ?: React.CSSProperties
+}
+
+
+const EducationItem: React.FC<EducationItemProps> = ({  date, school , degree , sgpa , style}) => (
+  <div className={`shadow-custom-box-xl  absolute py-3 px-10`} style={style}>
+      <span className='text-xl text-orange-500'>{date}</span>
+      <div className='pt-5 text-2xl font-semibold text-slate-700'>{school}</div>
+      <div className='text-lg pt-2 font-semibold text-slate-500'>{degree}</div>
+      <div className='pt-4   whitespace-pre-line'>{sgpa}</div>
+  </div>
+);
---
+++ frontend/src/components/timeline/EducationTimeLine.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+import { MdSchool } from "react-icons/md";
+
+export const  EducationTimeLine = () =>
+{
+    return (
+        <div className="">
+            <div className="education-timeline">
+                
+                <div className='z-10 pt-5'>    
+                    <TimeLineIcon
+                    icon={<MdSchool />}
+                    extraClasses="-left-5 top-6"
+                    />
+                </div>
+                <div className='z-10 pt-32'>
+                    <TimeLineIcon
+                    icon={<MdSchool />}
+                    extraClasses="-right-5 top-56"
+
+                    />
+                </div>
+                 <div className='z-10 pt-28'>
+                    <TimeLineIcon
+                    icon={<MdSchool />}
+                    extraClasses="-left-5 bottom-1"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const TimeLineIcon = ({icon , extraClasses} : {icon : ReactNode , extraClasses ?: string}) => (
+    
+      <div className="flex justify-center ">
+        <div className={`absolute  bg-gray-500 w-0.5 h-10 transform rotate-90 ${extraClasses}`}></div>
+        <div className='bg-customGray border border-gray-400 text-xl flex justify-center items-center w-12 h-12 rounded-full'>
+            {icon}
+        </div>
+      </div>
+      
+);
